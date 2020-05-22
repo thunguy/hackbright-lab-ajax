ecda12f4cb869acf5d78e9955a70d0f53c977b0f
Thu Nguyen <thunguy@me.com>
2020-05-21 21:36:15 -0700

    implement ajax calls on form

--- static/ajax-exercise.js
+++ static/ajax-exercise.js
@@ -2,32 +2,34 @@
 
 // Part 1
 
-$('#get-fortune-button').on('click', () => {
-  $.get('/replaceMe', (response) => {
+$("#get-fortune-button").on("click", () => {
+  $.get("/fortune", (response) => {
     //
     // This is the body of the callback function for $.get!
     // TODO: use `response` to update the text in `div#fortune-text`
     //
+
+    $("#fortune-text").html(response)
   });
 });
 
 
 // Part 2
 
-$('#weather-form').on('submit', (evt) => {
+$("#weather-form").on("submit", (evt) => {
   evt.preventDefault();
 
   const formData = {
     // TODO: select the zipcode input
-    zipcode: $('REPLACE THIS').val()
+    zipcode: $("#zipcode-field").val()
   };
 
   // TODO: choose a request method (GET or POST) by uncommenting one of
   // these blocks of code
 
-  // $.get('/replaceMe', formData, (response) => {
-  //   // Fill in the callback function
-  // });
+  $.get("/weather", formData, (response) => {
+    $("#weather-info").html(response.forecast)
+  });
 
   // $.post('/replaceMe', formData, (response) => {
   //   // Fill in the callback function
@@ -37,7 +39,7 @@ $('#weather-form').on('submit', (evt) => {
 
 // Part 3
 
-$("#order-form").on('submit', (evt) => {
+$("#order-form").on("submit", (evt) => {
   evt.preventDefault();
 
   // TODO: create an object to store key-value pairs that'll be sent to
@@ -48,4 +50,20 @@ $("#order-form").on('submit', (evt) => {
   // In the callback function, use the response from the server to
   // update #order-status. IMPORTANT: if the result code is 'ERROR',
   // make it show up in red.
+
+
+  const formData = {
+    melon_type: $("#melon-type-field").val(),
+    qty: $("#qty-field").val()
+  };
+
+  $.post("/order-melons", formData, (response) => {
+    if (response.code === "ERROR") {
+      $("#order-status").css("color", "red"); 
+    }
+    else { 
+      $("#order-status").css("color", "");
+    }
+    $("#order-status").html(response.msg);
+  })
 });
